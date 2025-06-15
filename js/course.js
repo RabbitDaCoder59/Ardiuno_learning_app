113e0f8a0bcc2bccabec2296e76130ce5114c0f4
Edeh chinedu <edehchinedu59@gmail.com>
2025-06-15 05:38:29 +0100

    made changes in the course and index page

--- js/course.js
+++ js/course.js
@@ -1,4 +1,17 @@
 function logout() {
   alert("Logging out...");
-  // Add actual logout logic here
+  // Add logout logic here
+}
+
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const icon = document.getElementById("toggleIcon");
+  sidebar.classList.toggle("collapsed");
+
+  // Change icon direction
+  if (sidebar.classList.contains("collapsed")) {
+    icon.classList.replace("fa-angle-left", "fa-angle-right");
+  } else {
+    icon.classList.replace("fa-angle-right", "fa-angle-left");
+  }
 }
